f76bc40496c91ff9a8c733b58633cc839a7d4342
Markus Mayer <widemeadows@gmail.com>
2008-05-16 22:07:10 +0000

    Added GPL v3 License information, as well as readme file and PHP file headers w/ SVN Id keyword.
    git-svn-id: file:///home/markus/dev/mine/legacy/wp-grain-code.svn/theme/trunk@5 6f6cd8cf-4e4d-0410-922c-de2976d17fed

--- archive.php
+++ archive.php
@@ -1,4 +1,28 @@
-<?php get_header(); ?>
+<?php 
+/*     
+	This file is part of Grain Theme for WordPress.
+
+    Grain Theme for WordPress is free software: you can redistribute it 
+	and/or modify it under the terms of the GNU General Public License 
+	as published by the Free Software Foundation, either version 3 of 
+	the License, or (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grain Theme for WordPress.  
+	If not, see <http://www.gnu.org/licenses/>.
+	
+	------------------------------------------------------------------
+	File version: $Id$	
+*/
+
+
+	get_header(); 
+?>
 
 	<div id="content" class="narrowcolumn">
 
