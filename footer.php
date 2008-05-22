c559bd0d8066cd77174298a3dc938db00685a602
Markus Mayer <widemeadows@gmail.com>
2008-05-22 22:47:58 +0000

    - CSS: footer edges and copyright line now moves automatically with footer height
    git-svn-id: file:///home/markus/dev/mine/legacy/wp-grain-code.svn/theme/trunk@27 6f6cd8cf-4e4d-0410-922c-de2976d17fed

--- footer.php
+++ footer.php
@@ -7,61 +7,63 @@
 ?>
 </div>
 
-<div id="footer">
+<div id="footer-complete">
 	<div id="footer-top"><div id="footer-top-inner" /></div>
+	<div id="footer">
 
-	<?php 
-		// embed the RSS icon
-		grain_embed_rss_icon();
+		<?php 
+			// embed the RSS icon
+			grain_embed_rss_icon();
+			
+			global $GrainOpt;
+			
+			// embed ATOM feed icon, if wanted
+			if($GrainOpt->getYesNo(GRAIN_FEED_ATOM_ENABLED)) grain_embed_atom_icon();
+
+			// embed Creative Commons license hint, if wanted
+			if( $GrainOpt->getYesNo(GRAIN_COPYRIGHT_CC_ENABLED) ) grain_embed_cc_div(); 
+		?>
+
+		<div id="footer-info<?php if( !$GrainOpt->getYesNo(GRAIN_COPYRIGHT_CC_ENABLED) ) echo '-padded'; ?>">
+
+			<span class="powered-by">
+				<?php
+				
+				$blogName = get_bloginfo('name');
+				$grainURL = '<a href="'.GRAIN_THEME_URL.'" title="Grain">Grain</a>';
+				$wordpressURL = '<a href="http://wordpress.org/">WordPress</a>';
+				$message = __("{BLOG} is proudly powered by {WP} and {GRAIN}");
+				echo str_replace( 
+					array( "{BLOG}", "{WP}", "{GRAIN}" ), 
+					array( $blogName, $wordpressURL, $grainURL ), 
+					$message );
+				
+				?>
+			</span>
+			
+			<span id="edit-post-link"><?php if( grain_getpostcount() ) edit_post_link(__("edit post", "grain"), ' | ', ''); ?></span>
+		</div>
+
+		<?php wp_footer(); ?>
 		
-		global $GrainOpt;
+	</div> <!-- footer -->
+	<div id="final-footer">
 		
-		// embed ATOM feed icon, if wanted
-		if($GrainOpt->getYesNo(GRAIN_FEED_ATOM_ENABLED)) grain_embed_atom_icon();
-
-		// embed Creative Commons license hint, if wanted
-		if( $GrainOpt->getYesNo(GRAIN_COPYRIGHT_CC_ENABLED) ) grain_embed_cc_div(); 
-	?>
-
-	<div id="footer-info<?php if( !$GrainOpt->getYesNo(GRAIN_COPYRIGHT_CC_ENABLED) ) echo '-padded'; ?>">
-
-		<span class="powered-by">
-			<?php
-			
-			$blogName = get_bloginfo('name');
-			$grainURL = '<a href="'.GRAIN_THEME_URL.'" title="Grain">Grain</a>';
-			$wordpressURL = '<a href="http://wordpress.org/">WordPress</a>';
-			$message = __("{BLOG} is proudly powered by {WP} and {GRAIN}");
-			echo str_replace( 
-				array( "{BLOG}", "{WP}", "{GRAIN}" ), 
-				array( $blogName, $wordpressURL, $grainURL ), 
-				$message );
-			
-			?>
-		</span>
+		<div id="syndication">
+			<?php 
+			if( $GrainOpt->getYesNo(GRAIN_SYND_FLAT_ENABLED) ) {
+				echo grain_flat_syndication( $GrainOpt->getYesNo(GRAIN_SYND_FLAT_DELIMITER) );
+			} ?>
+		</div>
 		
-		<?php if( grain_getpostcount() ) edit_post_link(__("edit post", "grain"), ' | ', ''); ?>
-
-		<div id="final-footer">
-			
-			<div id="syndication">
-				<?php 
-				if( $GrainOpt->getYesNo(GRAIN_SYND_FLAT_ENABLED) ) {
-					echo grain_flat_syndication( $GrainOpt->getYesNo(GRAIN_SYND_FLAT_DELIMITER) );
-				} ?>
-			</div>
-			
-			<div id="copyright">
-		       		<span><?php _e("Copyright", "grain"); ?> <?php grain_embed_copyright(TRUE); ?></span>
-			</div>
-			
-		 </div>
+		<div id="copyright">
+				<span><?php _e("Copyright", "grain"); ?> <?php grain_embed_copyright(TRUE); ?></span>
+		</div>
+		
 	</div>
+	<div id="footer-bottom"><div id="footer-bottom-inner"></div></div>
+</div> <!-- footer-complete -->
 
-	<?php wp_footer(); ?>
-	
-	<div id="footer-bottom"><div id="footer-bottom-inner"></div></div>	
-</div>
 </div>
 
 </div>
