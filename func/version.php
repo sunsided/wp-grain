2c5418633b15f88f2f2a3f64d587b269bbb4f127
Markus Mayer <widemeadows@gmail.com>
2008-05-30 01:36:17 +0000

    Changing version to "0.3 beta 1"
    git-svn-id: file:///home/markus/dev/mine/legacy/wp-grain-code.svn/theme/trunk@54 6f6cd8cf-4e4d-0410-922c-de2976d17fed

--- func/version.php
+++ func/version.php
@@ -7,7 +7,7 @@
 
 	/* defines */
 	
-	define('GRAIN_THEME_VERSION', '0.3');
+	define('GRAIN_THEME_VERSION', '0.3 beta 1');
 	define('GRAIN_THEME_VERSION_DEVBUILD', true);	
 	define('GRAIN_THEME_URL', 'http://wp-grain.wiki.sourceforge.net/');
 	define('GRAIN_YAPB_URL', 'http://wordpress.org/extend/plugins/yet-another-photoblog/');
