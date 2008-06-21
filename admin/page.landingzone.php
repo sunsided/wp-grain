c3ab5a8914134256a7eda1962306dc1bde89d898
Markus Mayer <widemeadows@gmail.com>
2008-06-21 02:23:56 +0000

    added error message in case of missing YAPB to the landing zone
    git-svn-id: file:///home/markus/dev/mine/legacy/wp-grain-code.svn/theme/trunk@148 6f6cd8cf-4e4d-0410-922c-de2976d17fed

--- admin/page.landingzone.php
+++ admin/page.landingzone.php
@@ -25,13 +25,28 @@
 	 * @since 0.3
 	 */
 	function grain_adminpage_landingzone() 
-	{		
-		grain_admin_inject_yapb_msg();
-		
+	{			
 	?>
 	<div class='wrap'>
 		<div id="grain-header">
 		<h2><?php echo str_replace("%VERSION", GRAIN_THEME_VERSION_BASE, __("Welcome to Grain %VERSION", "grain")); ?></h2>
+		<?php
+		if( !grain_is_yapb_installed() ):
+		?>
+		<div class="error">
+		<h3><?php _e("Missing the Yet Another Photoblog plugin", "grain") ?></h3>
+		<p><?php _e("Unfortunately the YAPB plugin could not be found. Reasons may be that it isn't installed yet or that it's just not activated.", "grain") ?><br />
+		<?php 
+			$msg = __("If you haven't installed it yet, you may want to download it <a href=\"%YAPB_URL\">here</a>. If you did not activate it, you can do so on the <a href=\"%PLUGINS_URL\">plugins</a> page.", "grain");
+			echo str_replace( array( "%YAPB_URL", "%PLUGINS_URL" ),
+							array( GRAIN_YAPB_URL, "./plugins.php" ),
+							$msg);
+		?></p>
+		<p><?php _e("You can continue editing the options for Grain now, but your photoblog won't work until YAPB is running.", "grain") ?></p>
+		</div>
+		<?php
+		endif;
+		?>
 		</div>
 	</div>
 	<?php 
